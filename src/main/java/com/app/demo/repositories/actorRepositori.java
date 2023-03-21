4f3a829eb53948eca295c59fac8a3106b0b2b1de
Marcela-Sevilla <sevillamarcela514@gmail.com>
2023-03-20 20:23:12 -0500

    Creando instancia del repositorio actor en el archivo de servicios

--- src/main/java/com/app/demo/repositories/actorRepositori.java
+++ src/main/java/com/app/demo/repositories/actorRepositori.java
@@ -1,8 +1,11 @@
 package com.app.demo.repositories;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import com.app.demo.models.actorModel;
+
 @Repository
-public interface actorRepositori {
+public interface ActorRepositori extends CrudRepository<actorModel, Long> {
     
 }
--- src/main/java/com/app/demo/services/actorServices.java
+++ src/main/java/com/app/demo/services/actorServices.java
@@ -1,5 +1,12 @@
 package com.app.demo.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.app.demo.repositories.ActorRepositori;
+
+@Service
 public class actorServices {
-    
+    @Autowired
+    ActorRepositori actorRepositori;
 }
