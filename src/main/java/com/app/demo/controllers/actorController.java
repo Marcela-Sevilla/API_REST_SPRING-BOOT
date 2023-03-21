d078ab88faa17b8cf78562154e8ba18a1882c0fe
Marcela-Sevilla <sevillamarcela514@gmail.com>
2023-03-20 20:34:09 -0500

    Creando controlador para las peticiones que se le hagan a la API

--- src/main/java/com/app/demo/controllers/actorController.java
+++ src/main/java/com/app/demo/controllers/actorController.java
@@ -1,5 +1,10 @@
 package com.app.demo.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/actor")
 public class actorController {
     
 }
--- src/main/java/com/app/demo/services/actorServices.java
+++ src/main/java/com/app/demo/services/actorServices.java
@@ -1,12 +1,25 @@
 package com.app.demo.services;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.app.demo.models.actorModel;
 import com.app.demo.repositories.ActorRepositori;
 
 @Service
 public class actorServices {
     @Autowired
     ActorRepositori actorRepositori;
+
+    public ArrayList<actorModel> obtenerActores(){
+        return (ArrayList<actorModel>) actorRepositori.findAll();
+    }
+
+    public actorModel guardarActor(actorModel actor){
+        return actorRepositori.save(actor);
+    }
+
+    
 }
