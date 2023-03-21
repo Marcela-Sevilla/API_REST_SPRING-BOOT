797a7f9256cc6d6cc9ea1d0c66e1f583f2c35718
Marcela-Sevilla <sevillamarcela514@gmail.com>
2023-03-20 20:38:47 -0500

    Rename to actorServices

--- src/main/java/com/app/demo/controllers/actorController.java
+++ src/main/java/com/app/demo/controllers/actorController.java
@@ -1,10 +1,14 @@
 package com.app.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.app.demo.services.actorServices;
 
 @RestController
 @RequestMapping("/actor")
 public class actorController {
-    
+    @Autowired
+    actorServices actorServices;
 }
---
+++ src/main/java/com/app/demo/controllers/actorServices.java
@@ -0,0 +1,5 @@
+package com.app.demo.controllers;
+
+public class actorServices {
+
+}
